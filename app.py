fe95822f80f164d0b1b02c6554deb0e7510252de
zc12345 <2104158177@qq.com>
2023-05-07 22:25:25 +0800

    [dev] add cache

--- app.py
+++ app.py
@@ -6,22 +6,19 @@ import streamlit as st
 
 from album import Album
 
-def run(args, album):
+@st.cache_resource
+def get_album(root_path, dump_path, backup_path):
+    album = Album(root_path, dump_path, backup_path)
+    return album
+
+def run(album):
     st.title("webui")
     st.write("上传一张图像或输入一个文本查询")
 
     # 输入区域
     query = st.text_input("输入文本查询")
     uploaded_file = st.file_uploader("上传一张图像", type=["jpg", "jpeg", "png"])
-    k = st.slider("选择一个整数值", 0, 20, 10, 1)
-
-    # 创建一个文件选择器，允许用户选择一个文件夹
-    # root_path = st.text_input("输入一个文件夹路径", "/path/to/folder")
-    # if not os.path.exists(root_path):
-    #     st.warning("文件夹不存在，请重新输入")
-    # else:
-    #     st.success(f"已选择文件夹：{root_path}")
-    #     album.update_db(root_path)
+    k = st.slider("选择最多展示多少张图片", 0, 20, 5, 1)
 
     # 处理输入并输出结果
     if uploaded_file is not None:
@@ -38,7 +35,7 @@ def run(args, album):
             img = Image.open(path)
             st.image(img, caption=f"{i} path:{path}, prob={prob}", use_column_width=True)
     else:
-        st.text("请上传一张图像或输入一个文本查询相似的图像。")
+        st.text("请上传一张图像或输入一个文本查询相似的图像")
         
 if __name__ == "__main__":
     # 定义模型参数
@@ -48,6 +45,6 @@ if __name__ == "__main__":
     parser.add_argument("--backup_path", type=str, default="backup")
     args = parser.parse_args()
     
-    album = Album(args.root_path, args.dump_path, args.backup_path)
+    album = get_album(args.root_path, args.dump_path, args.backup_path)
     
-    run(args, album)
+    run(album)
